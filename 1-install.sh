690be5b3d11fa68606aa4375f319c041c89e280d
John Leonard Castigo <39885989+CJohnLeonard@users.noreply.github.com>
2023-10-14 14:04:18 +0800

    Add files via upload

---
+++ 1-install.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+clear
+echo "    _             _       ___           _        _ _ "
+echo "   / \   _ __ ___| |__   |_ _|_ __  ___| |_ __ _| | |"
+echo "  / _ \ | '__/ __| '_ \   | || '_ \/ __| __/ _' | | |"
+echo " / ___ \| | | (__| | | |  | || | | \__ \ || (_| | | |"
+echo "/_/   \_\_|  \___|_| |_| |___|_| |_|___/\__\__,_|_|_|"
+echo ""
+echo "-----------------------------------------------------"
+echo ""
+echo "Important: Please make sure that you have followed the "
+echo "manual steps in the README to partition the harddisc!"
+echo "Warning: Run this script at your own risk."
+echo ""
+
+# ------------------------------------------------------
+# Enter partition names
+# ------------------------------------------------------
+lsblk
+read -p "Enter the name of the EFI partition (eg. sda1): " efipartition
+read -p "Enter the name of the ROOT partition (eg. sda2): " rootpartition
+# read -p "Enter the name of the VM partition (keep it empty if not required): " sda3
+
+# ------------------------------------------------------
+# Sync time
+# ------------------------------------------------------
+timedatectl set-ntp true
+
+# ------------------------------------------------------
+# Format partitions
+# ------------------------------------------------------
+mkfs.fat -F 32 /dev/$efipartition;
+mkfs.btrfs -f /dev/$rootpartition
+# mkfs.btrfs -f /dev/$sda3
+
+# ------------------------------------------------------
+# Mount points for btrfs
+# ------------------------------------------------------
+mount /dev/$rootpartition /mnt
+btrfs su cr /mnt/@
+btrfs su cr /mnt/@cache
+btrfs su cr /mnt/@home
+btrfs su cr /mnt/@snapshots
+btrfs su cr /mnt/@log
+umount /mnt
+
+mount -o compress=zstd:1,noatime,subvol=@ /dev/$rootpartition /mnt
+mkdir -p /mnt/{boot/efi,home,.snapshots,var/{cache,log}}
+mount -o compress=zstd:1,noatime,subvol=@cache /dev/$rootpartition /mnt/var/cache
+mount -o compress=zstd:1,noatime,subvol=@home /dev/$rootpartition /mnt/home
+mount -o compress=zstd:1,noatime,subvol=@log /dev/$rootpartition /mnt/var/log
+mount -o compress=zstd:1,noatime,subvol=@snapshots /dev/$rootpartition /mnt/.snapshots
+mount /dev/$efipartition /mnt/boot/efi
+# mkdir /mnt/vm
+# mount /dev/$sda3 /mnt/vm
+
+# ------------------------------------------------------
+# Install base packages
+# ------------------------------------------------------
+pacstrap -K /mnt base base-devel git linux linux-firmware vim openssh reflector rsync amd-ucode
+
+# ------------------------------------------------------
+# Generate fstab
+# ------------------------------------------------------
+genfstab -U /mnt >> /mnt/etc/fstab
+cat /mnt/etc/fstab
+
+# ------------------------------------------------------
+# Install configuration scripts
+# ------------------------------------------------------
+mkdir /mnt/archinstall
+cp 2-configuration.sh /mnt/archinstall/
+cp 3-yay.sh /mnt/archinstall/
+cp 4-zram.sh /mnt/archinstall/
+cp 5-timeshift.sh /mnt/archinstall/
+cp 6-preload.sh /mnt/archinstall/
+cp snapshot.sh /mnt/archinstall/
+
+# ------------------------------------------------------
+# Chroot to installed sytem
+# ------------------------------------------------------
+arch-chroot /mnt ./archinstall/2-configuration.sh
+
